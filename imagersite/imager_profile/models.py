99307073784e6a6f6e4e72779a136f5917142216
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-04-05 23:08:39 -0700

    Add ImageProfile model with fields, add to installed apps in settings

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,17 @@
+"""Models."""
 from django.db import models
 
+
 # Create your models here.
+
+
+class ImagerProfile(models.Model):
+    """Imager Profile Model."""
+
+    camera_model = models.CharField(max_length=200)
+    photography_type = models.TextField()
+    Friends = models.ManyToManyField('self')
+    Region = models.CharField(max_length=200)
+
+
+

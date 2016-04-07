5b10184e132004e2b9bd6424fc56cf7f4fc24716
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-04-06 23:32:45 -0700

    Add model manager, still needs work

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,5 +1,6 @@
 """Models."""
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -10,8 +11,24 @@ class ImagerProfile(models.Model):
 
     camera_model = models.CharField(max_length=200)
     photography_type = models.TextField()
-    friends = models.ManyToManyField('self')
+    # friends = models.ManyToManyField('self')
     region = models.CharField(max_length=200)
+    user = models.OneToOneField(User, unique=True, null=False)
+
+    def is_active(self):
+        """Return if the user can log in."""
+        return self.user.is_active
+
+
+class ActiveUserManager(models.Manager):
+    """Manager to grab active users."""
+
+    def get_query_set(self):
+        """Return only active users."""
+        return super(ActiveUserManager, self).get_query_set().filter(user.is_active())
+
+
+
 
 
 

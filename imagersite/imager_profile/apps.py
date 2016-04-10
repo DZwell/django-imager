5df942c5caf4c477d54d9fe78b88568cbec2793c
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-04-09 21:43:34 -0700

    Add code to let Django know about handlers

--- imagersite/imager_profile/apps.py
+++ imagersite/imager_profile/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ImagerProfileConfig(AppConfig):
     name = 'imager_profile'
+
+    def ready(self):
+        """Let app know about handlers when run."""
+        from imager_profile import handlers

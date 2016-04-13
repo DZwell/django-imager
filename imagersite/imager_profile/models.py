60e1b830a7e87e70ed9fec413ccef6ad73fd7dc5
Daniel Zwelling <dzwellingmusic@gmail.com>
2016-04-12 22:12:42 -0700

    Change user reference in tests, they pass

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -35,7 +35,7 @@ class ImagerProfile(models.Model):
 
     camera_model = models.CharField(max_length=200)
     photography_type = models.TextField()
-    friends = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='friend_of')
+    friends = models.ManyToManyField('self', related_name='friend_of')
     region = models.CharField(max_length=255, choices=REGIONS)
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -5,6 +5,8 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 import factory
 
+USER_BATCH_SIZE = 2
+
 
 class UserFactory(factory.django.DjangoModelFactory):
     """Set up a User factory."""
@@ -14,8 +16,8 @@ class UserFactory(factory.django.DjangoModelFactory):
 
         model = User
 
-    username = 'jordan'
-    email = '{}@exmaple.com'.format(username)
+    username = factory.Sequence(lambda n: 'user{}'.format(n))
+    email = factory.LazyAttribute(lambda a: '{}@sweet.com'.format(a.username))
 
 
 class SingleUserTests(TestCase):
@@ -28,14 +30,36 @@ class SingleUserTests(TestCase):
         self.user.save()
 
     def test_single_user_exists(self):
-        """Assert it exists."""
+        """Assert user has profile exists."""
         self.assertTrue(self.user.profile)
+
+    def test_user_is_properly_linked(self):
+        """Test."""
+        self.assertTrue(self.user.profile.user, self.user.username)
 
     def test_active_user(self):
         """Test active user."""
         self.assertTrue(self.user.profile.is_active)
 
 
-# assertTrue(self.photo in self.user.photos.all())
+class MultiUserFriendsTests(TestCase):
+    """Multiple users."""
+
+    def setUp(self):
+        """Set up multiple users."""
+        self.user0 = UserFactory.create()
+        self.user0.set_password('abc')
+        self.user0.save()
+
+        self.user1 = UserFactory.create()
+        self.user1.set_password('abc')
+        self.user1.save()
+
+        self.user0.profile.friends.add(self.user1.profile)
+
+    def test_friends(self):
+        """Test friends works."""
+        self.assertTrue(self.user1.profile in self.user0.profile.friends.all())
+
 
 
